4ed7a877e0d79c614821e26c59027329f13a0c4a
Eric Ihli <ericihli@gmail.com>
2016-10-11 21:26:09 -0700

    Add HTTP calls to divert and deliver
    Minitest uses a funny way to mock. We create a MiniTest::Mock, expect the :call method to be called with ['some args'] and return <some value>. Then we stub a class method and have it return the mock. If the second argument to stub responds to #call, then #call is sent to the method with ['some args']. That's why this works.

--- lib/destination.rb
+++ lib/destination.rb
@@ -1,22 +1,39 @@
+require 'http'
+
 class Destination
   class << self
-    def run(payload, validator)
+    def run(payload)
       # Instead of calling divert inside validate_inbound
       # we can raise and rescue here. Both validate_inbound
       # and deliver can fail. Unify error handling
-      return false unless validator.validate(payload)
-      deliver(payload)
-      true
+      if validate(payload)
+        deliver(payload)
+      else
+        divert(payload)
+      end
+    end
+
+    def validate(payload)
+      # Call to shared validator or define custom validation here
+      payload[:errors] ||= []
+      if payload[:package] == nil
+        payload[:errors] << "Package not in payload."
+        false
+      else
+        true
+      end
     end
 
     private
 
-    def divert
+    def divert(payload)
+      HTTP.post(payload[:package][:diversion_address])
       # Log to DB, send an email...
-      puts "Handle error"
+      puts payload[:errors]
     end
 
     def deliver(payload)
+      HTTP.post(payload[:package][:delivery_address])
       # Save to DB, POST to external API...
       puts "Make POST to API"
       # Rescue/throw
--- test/test_destination.rb
+++ test/test_destination.rb
@@ -1,21 +1,37 @@
 require 'minitest/autorun'
+require 'http'
 require_relative '../lib/destination'
 
 class TestDestination < MiniTest::Test
   def setup
-    @validator = Object.new
-    def @validator.validate(payload); end;
+    @payload = {
+      package: {}
+    }
   end
 
   def test_run_with_valid_payload
-    @validator.stub :validate, true do
-      assert Destination.run('some_payload', @validator) == true
+    mock = MiniTest::Mock.new
+    mock.expect :call, nil, ['success_url']
+
+    HTTP.stub :post, mock do
+      @payload[:package][:delivery_address] = "success_url"
+      Destination.run @payload
     end
+
+    mock.verify
   end
 
   def test_run_with_invalid_payload
-    @validator.stub :validate, false do
-      assert Destination.run('some_payload', @validator) == false
+    mock = MiniTest::Mock.new
+    mock.expect :call, nil, ['failure_url']
+
+    HTTP.stub :post, mock do
+      Destination.stub :validate, false do
+        @payload[:package][:diversion_address] = "failure_url"
+        Destination.run @payload
+      end
     end
+
+    mock.verify
   end
 end
