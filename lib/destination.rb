1339c8ffb10a9597d8153613ffb82900427bbfdf
Eric Ihli <ericihli@gmail.com>
2016-10-11 19:56:37 -0700

    Expand destination
    Extract validator so it can be mocked and re-used

--- lib/destination.rb
+++ lib/destination.rb
@@ -1,5 +1,25 @@
 class Destination
-  def self.run
-    true
+  class << self
+    def run(payload, validator)
+      # Instead of calling divert inside validate_inbound
+      # we can raise and rescue here. Both validate_inbound
+      # and deliver can fail. Unify error handling
+      return false unless validator.validate(payload)
+      deliver(payload)
+      true
+    end
+
+    private
+
+    def divert
+      # Log to DB, send an email...
+      puts "Handle error"
+    end
+
+    def deliver(payload)
+      # Save to DB, POST to external API...
+      puts "Make POST to API"
+      # Rescue/throw
+    end
   end
 end
--- test/test_destination.rb
+++ test/test_destination.rb
@@ -3,9 +3,19 @@ require_relative '../lib/destination'
 
 class TestDestination < MiniTest::Test
   def setup
+    @validator = Object.new
+    def @validator.validate(payload); end;
   end
 
-  def test_run
-    assert Destination.run == true
+  def test_run_with_valid_payload
+    @validator.stub :validate, true do
+      assert Destination.run('some_payload', @validator) == true
+    end
+  end
+
+  def test_run_with_invalid_payload
+    @validator.stub :validate, false do
+      assert Destination.run('some_payload', @validator) == false
+    end
   end
 end
