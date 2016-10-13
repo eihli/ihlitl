e059e3c5ffc87b678970c68aa3a43cac4d293737
Eric Ihli <ericihli@gmail.com>
2016-10-12 21:45:36 -0700

    Use helper deep copy instead of defining

--- test/test_transform.rb
+++ test/test_transform.rb
@@ -1,21 +1,11 @@
 require 'minitest/autorun'
+require_relative '../lib/helpers'
 require_relative '../lib/transform'
 
 class TestTransform < MiniTest::Test
   def setup
     @options = {}
     @payload = { package: {} }
-  end
-
-  def deep_copy(hash, accum = Hash.new)
-    hash.each do |key, value|
-      if value.is_a? Hash
-        accum[key] = deep_copy value
-      else
-        accum[key] = value
-      end
-    end
-    accum
   end
 
   def test_run_sends_payload_to_destination
