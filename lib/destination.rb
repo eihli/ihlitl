b4ae12993fac13e3a484434ef21021661ad0f37f
Eric Ihli <ericihli@gmail.com>
2016-10-13 21:02:02 -0700

    Refactor to simplify destination
    Validation should be handled by some other object.
    Treat destination the same way we are treating sources and trasforms with the only difference right now being it doesn't receive a destination. It is the destination.

--- lib/destination.rb
+++ lib/destination.rb
@@ -1,52 +1,10 @@
 require 'http'
 
 class Destination
-  class << self
-    def run(payload, options)
-      # Validate, deliver, or divert could blow up or fail.
-      # Perhaps throwing from validate/deliver and catching divert?
-      if validate(payload)
-        deliver(payload, options)
-      else
-        divert(payload, options)
-      end
-    end
+  def initialize
+  end
 
-    def validate(payload)
-      # Call to shared validator or define custom validation here
-      payload[:errors] ||= []
-
-      if payload[:package][:diversion_address] == nil
-        payload[:errors] << "Payload package requires a diversion address"
-      end
-
-      if payload[:package][:delivery_address] == nil
-        payload[:errors] << "Payload package requires a delivery address"
-      end
-
-      if payload[:package] == nil
-        payload[:errors] << "Package requires a payload"
-      end
-
-      if payload[:errors].length > 0
-        return false
-      else
-        return true
-      end
-    end
-
-    private
-
-    def divert(payload, options)
-      HTTP.post(payload, options)
-      # Log to DB, send an email...
-      puts payload[:errors]
-    end
-
-    def deliver(payload, options)
-      HTTP.post(payload, options)
-      # Save to DB, POST to external API...
-      puts "Make POST to API"
-    end
+  def run(payload)
+    payload
   end
 end
--- test/test_destination.rb
+++ test/test_destination.rb
@@ -4,37 +4,11 @@ require_relative '../lib/destination'
 
 class TestDestination < MiniTest::Test
   def setup
-    @options = {}
-    @payload = {
-      package: {}
-    }
+    @destination = Destination.new
   end
 
-  def test_run_with_valid_payload
-    mock = MiniTest::Mock.new
-    mock.expect :call, nil, [@payload, @options]
-
-    HTTP.stub :post, mock do
-      Destination.stub :validate, true do
-        @payload[:package][:delivery_address] = "success_url"
-        Destination.run @payload, @options
-      end
-    end
-
-    mock.verify
-  end
-
-  def test_run_with_invalid_payload
-    mock = MiniTest::Mock.new
-    mock.expect :call, nil, [@payload, @options]
-
-    HTTP.stub :post, mock do
-      Destination.stub :validate, false do
-        @payload[:package][:diversion_address] = "failure_url"
-        Destination.run @payload, @options
-      end
-    end
-
-    mock.verify
+  def test_default_delivery_behavior_returns_payload
+    payload = 'some_payload'
+    assert_equal @destination.run(payload), payload
   end
 end
