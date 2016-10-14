765728d980c32a2b4a28009e64a96c74546523dc
Eric Ihli <ericihli@gmail.com>
2016-10-13 22:05:44 -0700

    Modify how pipeline works.
    Use some recursion to generate a list of transforms.
    
    The thing is, the first needs to be instantiated with the instantion of the second, and the second the instantiation of the third.

--- lib/pipeline_manager.rb
+++ lib/pipeline_manager.rb
@@ -1,25 +1,39 @@
 class PipelineManager
-  @pipeline = []
-  @payload = {}
+  attr_accessor :transforms
 
-  class << self
-    attr_accessor :pipeline, :payload
+  def initialize(transforms, payload)
+    @payload = payload
+    @transforms = instantiate_transforms(transforms)
+  end
 
-    def run
-      deliver(process(payload))
-    end
-
-    def process(payload)
-      pipeline.each do |line|
-        transform = line[0]
-        args = [payload].concat line[1..-1]
-        payload = transform.run(*args)
+  def instantiate_transforms(transforms)
+    # if we only have a single transform, it is a destination
+    if transforms.length == 1
+      transform_def = transforms[0]
+      destination_class = transform_def[0]
+      if transform_def.length > 1 # we have args
+        args = transform_def[1..-1]
+        return [destination_class.new(args)]
+      else
+        return [destination_class.new]
       end
-      payload
+    else
+      transform_def = transforms[0]
+      destination_transform = instantiate_transforms(transforms[1..-1])
+      transform_class = transform_def[0]
+      if transform_def.length > 1 # we have args
+        args = transform_def[1..-1]
+        return transform_class.new(destination_transform, args)
+      else
+        return transform_class.new(destination_transform)
+      end
     end
+  end
 
-    def deliver(payload)
-      puts payload
+  def run
+    @transforms.each do |transform|
+      transform.run(@payload)
     end
   end
 end
+
--- test/test_pipeline_manager.rb
+++ test/test_pipeline_manager.rb
@@ -1,49 +1,23 @@
 require 'minitest/autorun'
 require_relative '../lib/helpers'
 require_relative '../lib/pipeline_manager'
+require_relative '../lib/transform'
 
 class TestPipelineManager < MiniTest::Test
   def setup
   end
 
-  def test_run_calls_transform_on_each_pipeline_item
-    mock_transforms = Array.new(3).map do |t|
-      mock = MiniTest::Mock.new
-      mock.expect :run, Hash.new, [Hash]
-      [mock]
-    end
-
-    PipelineManager.pipeline = mock_transforms
-    PipelineManager.run
-    mock_transforms.each do |m|
-      m[0].verify
-    end
-  end
-
-  def test_run_calls_deliver_with_transformed_payload
-    payload = {}
-    keys = [:a, :b, :c]
-    value = 0
-    mock_transforms = Array.new(3).map do |t|
-      transformed_payload = deep_copy payload
-      transformed_payload[keys[value]] = value
-      value += 1
-
-      mock = MiniTest::Mock.new
-      mock.expect :run, transformed_payload, [payload]
-
-      payload = transformed_payload
-      [mock]
-    end
-
-    PipelineManager.pipeline = mock_transforms
-
+  def test_a_single_transform_with_no_args
     mock = MiniTest::Mock.new
-    mock.expect :call, nil, [{a: 0, b: 1, c: 2}]
-    PipelineManager.stub :deliver, mock do
-      PipelineManager.run
-    end
+    destination = Object.new
+    mock.expect :new, destination
 
+    transforms = [[mock]]
+    pipeline = PipelineManager.new transforms, {}
     mock.verify
+
+    assert_equal pipeline.transforms[0], destination
   end
+
 end
+
