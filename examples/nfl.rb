60b6380a92839300f6474ec1f133588a412f18e0
Eric Ihli <ericihli@gmail.com>
2016-10-13 23:06:44 -0700

    Update NFL example

--- examples/nfl.rb
+++ examples/nfl.rb
@@ -1,31 +1,54 @@
+require_relative '../lib/transform'
+require_relative '../lib/destination'
 require_relative '../lib/pipeline_manager'
 
-class NFLTransform
-  class << self
-    def run(payload, api_key)
-      payload[:game_id] = 1
-      payload[:called_with_api_key] = api_key
-      payload
-    end
+class NFLTransform < Transform
+  def initialize(destination, api_key)
+    @api_key
+    super
+  end
+
+  def transform(payload)
+    payload[:game_id] = 1
+    payload[:called_with_api_key] = @api_key
+    payload
   end
 end
 
-class WeatherTransform
-  class << self
-    def run(payload, username, password)
-      payload[:temp_f] = '66.5'
-      payload[:credentials] = {username: username, password: password}
-      payload
-    end
+class WeatherTransform < Transform
+  def initialize(destination, username, password)
+    @username = username
+    @password = password
+    super
+  end
+
+  def transform(payload)
+    payload[:temp_f] = '66.5'
+    payload[:credentials] = {username: @username, password: @password}
+    payload
+  end
+end
+
+# Inherit from Destination?
+class SomeDestination < Destination
+  def deliver(payload)
+    puts 'Delivery sent!'
+    puts payload
   end
 end
 
 class NFLPipelineManager < PipelineManager
-  @payload = {}
-  @pipeline = [
-    [NFLTransform, '12345'],
-    [WeatherTransform, 'some_username', 'some_password']
-  ]
+  def deliver(payload)
+    SomeDestination.deliver(payload)
+  end
 end
 
-NFLPipelineManager.run
+pipeline = [
+  [NFLTransform, '12345'],
+  [WeatherTransform, 'some_username', 'some_password'],
+  [SomeDestination]
+]
+
+pipeline_manager = NFLPipelineManager.new pipeline, {}
+
+pipeline_manager.run
