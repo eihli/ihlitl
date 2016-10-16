26a0bd40f49146352986a5e99aafb3b1da63349a
Eric Ihli <ericihli@gmail.com>
2016-10-16 12:24:14 -0700

    Remove commented out code from example

--- examples/nfl.rb
+++ examples/nfl.rb
@@ -55,6 +55,7 @@ weather_clauses = [
     }
   }
 ]
+
 class WeatherFulfillmentAgent < IhliTL::FulfillmentAgent
   def run(payload)
     # Make post request
@@ -72,92 +73,3 @@ weather_contract = IhliTL::Contract.new(
 
 puts weather_contract.resolve({})
 
-# class WeatherContract < IhliTL::Contract
-#   def clauses
-#     [
-#       -> (subject) {
-#         subject[:zip_code].length == 5 &&
-#         Float(subject[:temp_f]) &&
-#         subject[:response_status] == '200'
-#       }
-#     ]
-#   end
-# end
-# 
-# class WeatherTransform < IhliTL::Transform
-#   def transform(payload)
-#     payload.merge!(ZipCodeContract.new(payload, -> (payload) {
-#       ZipCodeTransform.new.transform(payload)
-#     }).resolve)
-# 
-#     payload.merge!(TemperatureContract.new({zip_code: '94110'}, -> (payload) {
-#       TemperatureTransform.new.transform(payload)
-#     }).resolve)
-# 
-#     response = send_response_to_external_api
-#     payload[:response_status] = response
-#     payload
-#   end
-# 
-#   def send_response_to_external_api
-#     '200'
-#   end
-# end
-# 
-# class TemperatureContract < IhliTL::Contract
-#   def clauses
-#     [
-#       -> (subject) { temperature_clause(subject) }
-#     ]
-#   end
-# 
-#   def temperature_clause(subject)
-#     puts "Verifying weather"
-#     Float(subject[:temp_f]).class == Float
-#   end
-# end
-# 
-# class ZipCodeContract < IhliTL::Contract
-#   def clauses
-#     [
-#       -> (subject) { zip_code_clause(subject) }
-#     ]
-#   end
-# 
-#   def zip_code_clause(subject)
-#     puts "Verifying Zip Code"
-#     Integer(subject[:zip_code]) &&
-#       subject[:zip_code].length == 5
-#   end
-# end
-# 
-# class ZipCodeTransform < IhliTL::Transform
-#   def transform(payload)
-#     puts "Transforming Zip Code"
-#     payload[:zip_code] = '90210'
-#     payload
-#   end
-# end
-# 
-# class TemperatureTransform < IhliTL::Transform
-#   def transform(payload)
-#     payload = ZipCodeContract.new(payload, -> (payload) {
-#       ZipCodeTransform.new.transform(payload)
-#     }).resolve
-# 
-#     puts "Transforming Weather"
-#     payload[:temp_f] = '5'
-#     payload
-#   end
-# end
-# 
-# class OutboundTransform < IhliTL::Transform
-#   def transform(payload)
-#   end
-# end
-# 
-# foo = WeatherContract.new({}, -> (payload) {
-#   WeatherTransform.new.transform(payload)
-# })
-# 
-# puts foo.resolve
